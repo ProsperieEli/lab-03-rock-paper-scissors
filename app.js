895217deb9bf018b5c8f92e5c78ff940ec5eb15d
ProsperieEli <ElijahProsperie@gmail.com>
2021-08-18 19:54:50 -0400

    Finished project!!

--- app.js
+++ app.js
@@ -12,14 +12,31 @@ const myPercentageEl = document.querySelector('#percentage');
 // initialize global state
 let wins= 0;
 let loses = 0;
-
+let draw = 0;
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
-
   myButton.addEventListener('click', () => {
-    didUserWin();
-    getRandomThrow();
-    
+    let player = document.querySelector('input:checked');
+    let computer = getRandomThrow();
+    const winning = didUserWin(player.value, computer);
+    console.log(player.value, computer);
+
+    if (winning === 'You Win!'){
+      wins++;
+      myResultsEl.textContent = 'You Win!';
+    } else if( winning === 'You lose!'){
+      loses--;
+      myResultsEl.textContent = 'You Lose!';
+    } else {
+      draw ++;
+      myResultsEl.textContent = 'Draw!';
+    }
+
+    myWinsEl.textContent = `Wins!: ${wins}`;
+    myLosesEl.textContent = `Loses!: ${loses}`;
+    myDrawEl.textContent = `Draws!: ${draw}`;
   })
+
+ 
